f4e10ad5829d49f9bab1590f76ca789eed501e15
unknown <20241ctb0180010@estudantes.ifpr.edu.br>
2026-08-05 12:17:44 -0300

    JanelaEmProgesso

--- SeuAdemirUnity/Assets/PontoFinal/Scripts/Janela.cs
+++ SeuAdemirUnity/Assets/PontoFinal/Scripts/Janela.cs
@@ -3,27 +3,32 @@ using UnityEngine;
 public class Janela : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-   [SerializeField] private Animator animator;
-    
-    // Nome do parâmetro exatamente como criado no Animator
-    private string triggerName = "Interagir";
+    [SerializeField] private Animator animator;
+    [SerializeField] private GameObject objetoParaAtivar;
+    [SerializeField] private string triggerName = "Interagir";
 
     private void Awake()
     {
-        // Caso o Animator esteja no próprio GameObject e não tenha sido arrastado no Inspector
         if (animator == null)
         {
             animator = GetComponent<Animator>();
         }
     }
-
-    // Chame este método onde a sua lógica de interação já é executada
     public void Interagir()
     {
         if (animator != null)
         {
             animator.SetTrigger(triggerName);
         }
+    }
+
+    public void AtivarOutroObjeto()
+    {
+        if (objetoParaAtivar != null)
+        {
+            objetoParaAtivar.SetActive(true);
+        }
+
     }
 
 }
