e030bc2d06e3c5d289601fab16e028955526dedd
unknown <20241ctb0180010@estudantes.ifpr.edu.br>
2026-08-13 12:21:47 -0300

    nn acaba luz pfv

--- SeuAdemirUnity/Assets/PontoFinal/Scripts/Janela.cs
+++ SeuAdemirUnity/Assets/PontoFinal/Scripts/Janela.cs
@@ -4,22 +4,11 @@ using UnityEngine;
 public class Janela : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    [SerializeField] private Animator animator;
     [SerializeField] private GameObject objetoParaAtivar;
-    [SerializeField] private string triggerName = "Interagir";
     [SerializeField] private float NHEEEUU;
-
     [SerializeField] private Vector3 finalPosition;
     private bool jaFoiAtivado = false;
 
-
-    private void Awake()
-    {
-        if (animator == null)
-        {
-            animator = GetComponent<Animator>();
-        }
-    }
     public IEnumerator Interagir()
     {
         finalPosition = new Vector3 (transform.position.x, transform.position.y, transform.position.z + 1);
