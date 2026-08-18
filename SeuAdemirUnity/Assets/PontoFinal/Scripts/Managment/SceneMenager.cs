ee5e304b2865cbaee1e97897af37747e3d348def
unknown <20241ctb0180010@estudantes.ifpr.edu.br>
2026-08-18 11:19:00 -0300

    ai

--- SeuAdemirUnity/Assets/PontoFinal/Scripts/Managment/SceneMenager.cs
+++ SeuAdemirUnity/Assets/PontoFinal/Scripts/Managment/SceneMenager.cs
@@ -6,7 +6,8 @@ public class SceneMenager : MonoBehaviour
 {
     [SerializeField] private string sceneCine; // Nome da cena a ser carregada
     [SerializeField] private string sceneGame; // Nome da cena a ser carregada
-    [SerializeField] private float changeTime; 
+    [SerializeField] private string sceneMenu; // Nome da cena a ser carregada
+    [SerializeField] private float changeTime;
     public static SceneMenager Instance { get; private set; }
 
     private void Awake()
@@ -20,14 +21,18 @@ public class SceneMenager : MonoBehaviour
         Instance = this;
         DontDestroyOnLoad(gameObject);
     }
-public void LoadGame ()
+    public void LoadGame()
     {
-        SceneManager.LoadScene (sceneGame); // Ir para a cena de jogo
+        SceneManager.LoadScene(sceneGame); // Ir para a cena de jogo
     }
-    public void LoadCinematic ()
+    public void LoadCinematic()
     {
-        SceneManager.LoadScene (sceneCine); // Ir para a cinematic
-        StartCoroutine(CinematicTime(sceneGame, changeTime)); // Iniciar a corrotina para carregar a cena cinematica após o tempo definido
+        SceneManager.LoadScene(sceneCine); // Ir para a cinematic
+        StartCoroutine(CinematicTime(sceneGame, changeTime)); // Iniciar a corrotina para carregar a cena cinematica apï¿½s o tempo definido
+    }
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(sceneMenu); // Ir para a cena de jogo
     }
     public IEnumerator CinematicTime(string nextScene, float cinematicTime)
     {
