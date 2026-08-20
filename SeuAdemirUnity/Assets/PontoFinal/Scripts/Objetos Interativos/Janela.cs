e1407e238834d9d82637a50e8e22f74f2d84f202
unknown <>
2026-08-20 13:19:48 -0300

    baguncinhahihihi

--- SeuAdemirUnity/Assets/PontoFinal/Scripts/Objetos Interativos/Janela.cs
+++ SeuAdemirUnity/Assets/PontoFinal/Scripts/Objetos Interativos/Janela.cs
@@ -5,19 +5,20 @@ public class Janela : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] private GameObject objetoParaAtivar;
-    [SerializeField] private float NHEEEUU;
-    [SerializeField] private Vector3 finalPosition;
+    [SerializeField] private float velocidade;
+    private Vector3 finalPosition;
+    [SerializeField] private float sidePositionsZ;
     private bool jaFoiAtivado = false;
 
     public IEnumerator Interagir()
     {
-        finalPosition = new Vector3 (transform.position.x, transform.position.y, transform.position.z + 1);
+        finalPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z + sidePositionsZ);
         float time = 0;
 
         while (time < 1)
         {
             time += Time.deltaTime;
-            transform.position = Vector3.Lerp(transform.position, finalPosition, time * NHEEEUU);
+            transform.position = Vector3.Lerp(transform.position, finalPosition, time * velocidade);
             yield return null;
         }
         transform.position = finalPosition;
