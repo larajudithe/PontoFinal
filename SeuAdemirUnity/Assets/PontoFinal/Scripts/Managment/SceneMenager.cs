412f112c01345f49b4f19fc3d591188a6020385d
unknown <20241ctb0180010@estudantes.ifpr.edu.br>
2026-08-25 11:15:13 -0300

    tens o que é nescssario...??

--- SeuAdemirUnity/Assets/PontoFinal/Scripts/Managment/SceneMenager.cs
+++ SeuAdemirUnity/Assets/PontoFinal/Scripts/Managment/SceneMenager.cs
@@ -9,6 +9,8 @@ public class SceneMenager : MonoBehaviour
     [SerializeField] private string sceneGame; // Nome da cena a ser carregada
     [SerializeField] private string sceneMenu; // Nome da cena a ser carregada
     [SerializeField] private float changeTime;
+    [SerializeField] private GameObject painelOPC;
+    [SerializeField] private GameObject painelMENU;
     private Coroutine cineTimerCoroutine;
     private InputAction skipCine;
     public static SceneMenager Instance { get; private set; }
@@ -48,10 +50,29 @@ public class SceneMenager : MonoBehaviour
         Debug.Log("Game");
         SceneManager.LoadScene(sceneMenu); // Ir para a cena de jogo
     }
+
+    public void AbrirOPC()
+    {
+        painelMENU.SetActive(false);
+        painelOPC.SetActive(true);
+    }
+
+    public void FecharOPC()
+    {
+        painelMENU.SetActive(true);
+        painelOPC.SetActive(false);
+    }
+
     public void Sair()
     {
-        Debug.Log("Sair");
+        
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
     }
+
     private IEnumerator CinematicTime(string nextScene, float cinematicTime)
     {
         Debug.Log("Esta contando o tempo da cinematic");
