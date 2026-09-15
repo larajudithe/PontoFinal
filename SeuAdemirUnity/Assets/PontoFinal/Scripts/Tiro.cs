c3d39984fa4e0d09bd0683d16ee38be2cd64ed58
NoxKa <20241ctb0180015@estudantes.ifpr.edu.br>
2026-09-15 17:36:30 -0300

    Revert "AB"
    This reverts commit 057ec28a4803d58ca86fe8ecbe55bbbf07c2a832.

---
+++ SeuAdemirUnity/Assets/PontoFinal/Scripts/Tiro.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using FMODUnity;
+public class Tiro : MonoBehaviour
+{
+    // [SerializeField] private StudioEventEmitter extintorSound;
+
+    // // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // void Start()
+    // {
+    //     extintorSound.Play();
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+}
