59808a04306df2ebcea74dd835c0a4d7ff3f513c
Guilherme <20241ctb0180020@estudantes.ifpr.edu.br>
2026-09-15 15:34:05 -0300

    InMyLife

--- SeuAdemirUnity/Assets/PontoFinal/Scripts/FireClone.cs
+++ SeuAdemirUnity/Assets/PontoFinal/Scripts/FireClone.cs
@@ -12,10 +12,10 @@ public class FireClone : MonoBehaviour
     GameObject clone;
     GameObject clone2;
     [SerializeField] private GameObject Puzzle;
-    [SerializeField] float SomaPosicaoZ = 1f;
-    [SerializeField] int SomaFogo = 7;
-    [SerializeField] int DiminuirFogo = 1;
-    [SerializeField] int fogo = 7;
+    private float SomaPosicaoZ = 0.7f;
+    private int SomaFogo = 7;
+    private int DiminuirFogo = 1;
+    private int fogo = 7;
 
     void Start()
     {
